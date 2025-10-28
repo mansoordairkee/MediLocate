e1a35dc80e5a838da74c2b965819ed7958bb2e4c
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2025-10-28 14:04:58 +0530

    Auth Routing added

--- user-facility-routes/authRoutes.js
+++ user-facility-routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../auth-controllers/authController');
+const {signUp, login} = require('../auth-controllers/authController');
 
 router.post('/signup', signUp);
 router.post('/login', login);
