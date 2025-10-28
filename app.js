44c28cc825cc180eb55f1bcb44b7acd0d3446a2a
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2025-10-28 14:43:31 +0530

    Changes Made

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const db = require("./database-config/db"); 
 
 const userRoutes = require("./user-facility-routes/userRoutes");
 const facilityRoutes = require("./user-facility-routes/facilityRoutes");
+const authRoutes = require("./user-facility-routes/authRoutes");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
-app.use("/users", userRoutes);
-app.use("/facilities", facilityRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/facilities", facilityRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to MediLocate Backend API");
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to the database.");
+  }
+});
 
 //START SERVER 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
