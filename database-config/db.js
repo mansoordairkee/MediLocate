deee3175dcc712c49afe43e3b974a677008178ac
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-04-17 18:47:15 +0530

    Database modified

--- database-config/db.js
+++ database-config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "mediloc@123",
-  database: "mediloc",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
